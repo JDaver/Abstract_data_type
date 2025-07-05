dc09c4a2f9ea92930d2c13e5925cbf10775d4f3c
JDaver <gundogdudaver@gmail.com>
2025-07-05 22:07:25 +0200

    add deletePQ(), isSet(), extractMax() and the Heapify()

--- Sources/priorityQueue.c
+++ Sources/priorityQueue.c
@@ -14,6 +14,37 @@ static int isFull(priorityQueue* q){
     else return 1;
 }
 
+static int isSet(priorityQueue* q){
+    if(q==NULL) return 0;
+    else return 1;
+}
+
+static void heapify(priorityQueue* q, int i){
+    int tmp = 0;
+    while(1){
+        int largest = i;
+        int left = 2*i +1;
+        int right = 2*i +2;
+
+        if(left < q->size && q->array[left] > q->array[largest]){
+            largest = left;
+        }
+        if(right < q->size && q->array[right] > q->array[largest]){
+            largest = right;
+        }
+
+        if(largest!=i){
+            tmp = q->array[i];
+            q->array[i] = q->array[largest];
+            q->array[largest] = tmp;
+            i = largest;
+        }else{
+            break;
+        }
+        }
+    }
+
+
 void initPQ(priorityQueue* q, int max){
     q->array = malloc(max*sizeof(int));
     q->capacity = max;
@@ -21,6 +52,10 @@ void initPQ(priorityQueue* q, int max){
 }
 
 void printPQ (priorityQueue* q){
+    if(isSet(q)==0){
+        printf("the priorityQueue does not exist!\n");
+        return;
+    } 
     for(int i =0; i< q->size; i++){
         printf(" elem: %d\n",q->array[i]);
     }
@@ -44,4 +79,25 @@ priorityQueue* insert(priorityQueue* q, int val){
     }
     return  q;
 }
+
+priorityQueue* extractMax(priorityQueue* q){
+    if(isEmpty(q)==1){
+        printf("Error, data structure is empty\n");
+        return q;
+    }
+    q->size--;
+    q->array[0] = q->array[q->size];
+    heapify(q,0);
+    return q;
+}
+
+void deletePQ(priorityQueue** q){
+    if(q && *q){
+        free((*q)->array);
+        free(*q);
+        *q = NULL;
+    }
+    
+}
+
 
